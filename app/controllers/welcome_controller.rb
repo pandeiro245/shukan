c672b46814adbd100830bdef360fecdb1a3573ae
Nishikokura Hironobu <nishiko@mindia.jp>
2021-03-21 05:39:26 +0000

    やったボタン押した日はトップへのアクセスもえらい画面に転送

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,10 @@ class WelcomeController < ApplicationController
     if current_user.present?
       if current_user.goals.blank?
         redirect_to new_goal_path
-      else
+      elsif current_user.yet?
         redirect_to goal_path(current_user.goals.last)
+      else
+        redirect_to events_path
       end
     end
   end
