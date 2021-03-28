23de8eeddb22c93ed08d38c3ef4260fe1c549723
Nishikokura Hironobu <nishiko@mindia.jp>
2021-03-28 06:03:33 +0000

    月曜日スタートでその週の初回アクセスはやることを選びなおせる refs #25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
   def current_user
     return nil if session[:user_id].blank?
     @current_user ||= User.find(session[:user_id])
